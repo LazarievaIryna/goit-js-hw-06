a68b1c60e21e4ab6b164cb968ab9deb0cb2fc8da
Iryna Lazareva <il337733@gmail.com>
2022-08-03 19:51:03 +0300

    add task 3

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -3,22 +3,9 @@ console.log(`Number of categories: ${categoriesEl}`);
 
 
 
-// по частям
-const categoriesChildren = document.querySelector(`#categories`);
-const listItem = categoriesChildren.querySelectorAll(`h2`);
-console.log(listItem);
+const listTitleEl = document.querySelectorAll(".item h2");
 
-listItem.forEach((element) => console.log(element.textContent));
-
-const ulListEl = document.querySelectorAll(`.item ul`);
-ulListEl.forEach((list) => console.log(list.children.length));
-
-///////
-
-
-const tittleEl = document.querySelectorAll(".item h2");
-tittleEl.forEach((element) => {
+listTitleEl.forEach((element) => {
   console.log(`Category: ${element.textContent}`);
   console.log(`Elements: ${element.nextElementSibling.children.length}`);
-  console.log();
 });
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("#ingredients");
+
+
+const element = ingredients.map((option) => {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.textContent = option;
+  ingredientItem.classList.add("item");
+  return ingredientItem;
+});
+listEl.append(...element);
