a57021ed8ddbdc3856addd0f453a7d5a585445cb
Iryna Lazareva <il337733@gmail.com>
2022-08-07 18:00:45 +0300

    change task 5

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,11 +1,3 @@
-// const inputText = document.querySelector(`#name-input`);
-// const output = document.querySelector(`#name-output`);
-// const text = "Anonymous";
-// output.textContent = text;
-// inputText.addEventListener("input", (evn) => {
-//   output.textContent = evn.currentTarget.value;
-// });
-
 const inputText = document.querySelector(`#name-input`);
 const output = document.querySelector(`#name-output`);
 
